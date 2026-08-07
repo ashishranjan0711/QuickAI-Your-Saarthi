50e11caef579b6e8c0eda4bebb726d5effebf5ca
ashish123-bit <168567835+ashish123-bit@users.noreply.github.com>
2026-08-07 23:59:19 +0530

    changes in tools improving ui

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -10,8 +10,9 @@ const Hero = () => {
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
 
         <div className='text-center mb-6'>
-            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content <br/> with <span className='text-primary'>AI tools</span></h1>
-            <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'>Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow.</p>
+            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Turn Your Ideas Into Amazing Content <br/> with <span className='text-primary'>AI tools</span></h1>
+            <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'>Transform your ideas into exceptional content with powerful AI tools. Create engaging articles, generate stunning visuals, and streamline your entire workflow—all in one intelligent platform.
+</p>
         </div>
 
         <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
